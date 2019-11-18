60ea8e53183f10e571d67d83955cacfb055764af
daliyajohn <daliyajohn75@gmail.com>
2019-11-18 16:14:20 +0530

    feat(user-list): implement search by name

--- src/app/home/user-list/user-list.component.ts
+++ src/app/home/user-list/user-list.component.ts
@@ -76,4 +76,47 @@ export class UserListComponent implements OnInit {
     // document.querySelector('.modal-backdrop').classList.toggle('d-none');
     this.editUser = false;
   }
+
+  // search data 
+  filterValue() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("name");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("userTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 }
