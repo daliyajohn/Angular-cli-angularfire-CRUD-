aefb9941edbc4f41b34f638fa5c3afc7eb6c87a3
daliyajohn <daliyajohn75@gmail.com>
2019-11-19 10:26:03 +0530

    feat(theme): add task list component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,10 @@ import { HomeComponent } from './home.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { HomeService } from './service/home.service';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
-  declarations: [HomeComponent, UserListComponent, UserFormComponent],
+  declarations: [HomeComponent, UserListComponent, UserFormComponent, TaskListComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
