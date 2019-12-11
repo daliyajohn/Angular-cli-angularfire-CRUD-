747bc8929a8edc99d14b60e51975e9a8566d2032
daliyajohn <daliyajohn75@gmail.com>
2019-12-11 12:47:29 +0530

    feat(login): code cleanup

--- src/app/home/service/home.service.ts
+++ src/app/home/service/home.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { AngularFireAuth } from "@angular/fire/auth";
 import { Router } from '@angular/router';
